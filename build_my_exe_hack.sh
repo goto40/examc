17e21ec928b2f7bc2a0ed5c85a724dd4127dfe06
goto40 <pierre.bayerl@googlemail.com>
2018-12-23 16:11:40 +0100

    (minor change)

--- build_my_exe_hack.sh
+++ build_my_exe_hack.sh
@@ -1,5 +1,6 @@
 pip install pyinstaller || exit 1
-python setup.py install || exit 1
+#python setup.py install || exit 1
+export PYTHONPATH=.
 pyinstaller --onefile examc/console.py --add-data 'examc/Exercise.tx:examc' --add-data 'examc/Config.tx:examc' --add-data 'examc/Exam.tx:examc' --add-data 'examc/master.tex.template:examc' -n examc || exit 1
 find dist/examc || exit 1
 echo "copy the exe to your favorite path"
