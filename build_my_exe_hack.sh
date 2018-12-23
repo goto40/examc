6768e1a200a7f750a6932c4ce321a7551ef8a561
goto40 <pierre.bayerl@googlemail.com>
2018-12-23 15:34:48 +0100

    fix

--- build_my_exe_hack.sh
+++ build_my_exe_hack.sh
@@ -1,6 +1,7 @@
 pip install pyinstaller
-export PYTHONPATH=.
-pyinstaller --onefile examc/console.py -n examc
+python setup.py install
+pyinstaller --onefile examc/console.py --add-data 'examc/Exercise.tx:examc' --add-data 'examc/Config.tx:examc' --add-data 'examc/Exam.tx:examc' --add-data 'examc/master.tex.template:examc' -n examc
 find dist/examc
 echo "copy the exe to your favorite path"
 echo "cp dist/examc ~/Documents/tools"
+
